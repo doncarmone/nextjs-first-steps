ba71afc9951c0a5a5d44fdda79be176bc22fc83a
Javier Carmona <javier.carmona@wizeline.com>
2025-06-18 10:25:52 -0600

    [FIX] Fixed layout

--- app/(general)/about/layout.tsx
+++ app/(general)/about/layout.tsx
@@ -5,16 +5,3 @@ export default function AboutLayout({
 }) {
   return <div>{children}</div>;
 }
-
-
-curl -X POST 'http://localhost:8086/api/v2/delete?org=IR&bucket=default' \
-  -H "Authorization: lpec3m3nY7p5S0bSIZG5rlDZYX3x6bNwDRFknG7ZTBvcn-Qs46oTx4SpdrQCfRrHWiUKif2DQLMnWshrzw9pcA==" \
-  -H 'Content-Type: application/json' \
-  -d '{
-    "start": "1970-01-01T00:00:00Z",
-    "stop": "2025-06-01T00:00:00Z",
-  }'
-
-  influx auth list
-
-  ID: 7950d1f94132ec8d
